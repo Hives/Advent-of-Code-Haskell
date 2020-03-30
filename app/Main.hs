74e821984878cd1695c806bd9eac10edb9cd0dac
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-03-30 01:37:00 +0100

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,13 @@ module Main where
 
 import           Day1
 import           Day2
+import           Day3
 
 main :: IO ()
 main = do
-  putStrLn ("Day 1 part 1: " ++ show day1Part1)
-  putStrLn ("Day 1 part 2: " ++ show day1Part2)
-  putStrLn ("Day 2 part 1: " ++ show day2Part1)
-  putStrLn ("Day 2 part 2: " ++ show day2Part2)
+  -- putStrLn ("Day 1 part 1: " ++ show day1Part1)
+  -- putStrLn ("Day 1 part 2: " ++ show day1Part2)
+  -- putStrLn ("Day 2 part 1: " ++ show day2Part1)
+  -- putStrLn ("Day 2 part 2: " ++ show day2Part2)
+  putStrLn ("Day 3 part 1: " ++ show day3Part1) -- very slow!
 
--- src/Day3.hs
+++ src/Day3.hs
@@ -2,6 +2,9 @@ module Day3 where
 
 import           Day3Input
 
+data Point = Point Int Int
+             deriving (Show, Eq)
+
 data Section = RRight Int
              | UUp Int
              | LLeft Int
@@ -15,26 +18,34 @@ toSections = map (\x -> f x) where
              | c == 'L' = LLeft (read cs :: Int)
              | c == 'D' = DDown (read cs :: Int)
 
-sectionToCoordinates :: Section -> (Int, Int) -> [(Int, Int)]
-sectionToCoordinates section (x, y) = case section of
-  (RRight n) -> map (\i -> (x + i, y)) [1 .. n]
-  (UUp    n) -> map (\i -> (x, y + i)) [1 .. n]
-  (LLeft  n) -> map (\i -> (x - i, y)) [1 .. n]
-  (DDown  n) -> map (\i -> (x, y - i)) [1 .. n]
+sectionToPoints :: Section -> Point -> [Point]
+sectionToPoints section (Point x y) = case section of
+  (RRight n) -> map (\i -> (Point (x + i) y)) [1 .. n]
+  (UUp    n) -> map (\i -> (Point x (y + i))) [1 .. n]
+  (LLeft  n) -> map (\i -> (Point (x - i) y)) [1 .. n]
+  (DDown  n) -> map (\i -> (Point x (y - i))) [1 .. n]
 
-wireToCoordinates :: [String] -> [(Int, Int)]
-wireToCoordinates wire = f (toSections wire) (0, 0) []
+wireToPoints :: [String] -> [Point]
+wireToPoints wire = f (toSections wire) (Point 0 0) []
  where
   f [] _ acc = acc
-  f (w : ws) (x, y) acc =
-    let newSection = sectionToCoordinates w (x, y)
+  f (w : ws) (Point x y) acc =
+    let newSection = sectionToPoints w (Point x y)
     in  f ws (last newSection) (acc ++ newSection)
 
-manhattanDistance :: (Int, Int) -> Int
-manhattanDistance (x, y) = abs x + abs y
+manhattanDistance :: Point -> Int
+manhattanDistance (Point x y) = abs x + abs y
+
+example1 = ["R8", "U5", "L5", "D3"]
+example2 = ["U7", "R6", "D4", "L4"]
 
 day3Part1 :: Int
 day3Part1 =
-  let (wire1, wire2) =
-          (wireToCoordinates puzzleInput1, wireToCoordinates puzzleInput2)
+  let (wire1, wire2) = (wireToPoints puzzleInput1, wireToPoints puzzleInput2)
   in  minimum . (map manhattanDistance) $ (filter (\n -> n `elem` wire2) wire1)
+
+index :: [a] -> [(a, Int)]
+index xs = zip xs [1 ..]
+
+day3Part2 :: Int
+day3Part2 = undefined
