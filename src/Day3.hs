ec5089eb52277595400cc06ab7ac2d110f82678e
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-03-31 01:14:41 +0100

    Refactor d03p01 - this runs like a minute faster
    Takes about 3m30s on this machine

--- src/Day3.hs
+++ src/Day3.hs
@@ -12,7 +12,7 @@ data Section = RRight Int
              deriving Show
 
 toSections :: [String] -> [Section]
-toSections = map (\x -> f x) where
+toSections = map f where
   f (c : cs) | c == 'R' = RRight (read cs :: Int)
              | c == 'U' = UUp (read cs :: Int)
              | c == 'L' = LLeft (read cs :: Int)
@@ -20,10 +20,10 @@ toSections = map (\x -> f x) where
 
 sectionToPoints :: Section -> Point -> [Point]
 sectionToPoints section (Point x y) = case section of
-  (RRight n) -> map (\i -> (Point (x + i) y)) [1 .. n]
-  (UUp    n) -> map (\i -> (Point x (y + i))) [1 .. n]
-  (LLeft  n) -> map (\i -> (Point (x - i) y)) [1 .. n]
-  (DDown  n) -> map (\i -> (Point x (y - i))) [1 .. n]
+  (RRight n) -> map (\i -> Point (x + i) y) [n, (n - 1) .. 1]
+  (UUp    n) -> map (\i -> Point x (y + i)) [n, (n - 1) .. 1]
+  (LLeft  n) -> map (\i -> Point (x - i) y) [n, (n - 1) .. 1]
+  (DDown  n) -> map (\i -> Point x (y - i)) [n, (n - 1) .. 1]
 
 wireToPoints :: [String] -> [Point]
 wireToPoints wire = f (toSections wire) (Point 0 0) []
@@ -31,7 +31,7 @@ wireToPoints wire = f (toSections wire) (Point 0 0) []
   f [] _ acc = acc
   f (w : ws) (Point x y) acc =
     let newSection = sectionToPoints w (Point x y)
-    in  f ws (last newSection) (acc ++ newSection)
+    in  f ws (head newSection) (newSection ++ acc)
 
 manhattanDistance :: Point -> Int
 manhattanDistance (Point x y) = abs x + abs y
@@ -42,7 +42,8 @@ example2 = ["U7", "R6", "D4", "L4"]
 day3Part1 :: Int
 day3Part1 =
   let (wire1, wire2) = (wireToPoints puzzleInput1, wireToPoints puzzleInput2)
-  in  minimum . (map manhattanDistance) $ (filter (\n -> n `elem` wire2) wire1)
+  -- let (wire1, wire2) = (wireToPoints example1, wireToPoints example2)
+  in  minimum . map manhattanDistance $ filter (`elem` wire2) wire1
 
 index :: [a] -> [(a, Int)]
 index xs = zip xs [1 ..]
